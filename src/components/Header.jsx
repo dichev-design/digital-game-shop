9b49357567f6ce5f43fab61ba6c007bc4ec0c2aa
dichev-design <dichev.official@gmail.com>
2024-12-10 18:45:43 +0200

    Header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import "./header.css";
+import userImg from '../images/profile.JPG';
 
 export default function () {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <a href="#" className="menu">
+        <i class="bi bi-sliders"></i>
+      </a>
+      <div className="userItems">
+        <a href="#" className="icon">
+          <i class="bi bi-heart-fill"></i>
+          <span className="like">0</span>
+        </a>
+        <a href="#" className="icon">
+          <i class="bi bi-bag-fill"></i>
+            <span className="bag">0</span>
+        </a>
+        <div className="avatar">
+          <a href="#">
+            <img src={userImg} alt="User Image" />
+            <div className="user">
+              <span>User Name</span>
+              <a href="">View Profile</a>
+            </div>
+          </a>
+        </div>
+      </div>
+    </header>
   )
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './main.css';
 import SideMenu from '../components/SideMenu';
+import Header from '../components/Header';
 
 export default function Main() {
   return (
     <main>
         <SideMenu/>
         <div className="banner">
-          
+          <Header/>
         </div>
     </main>
   )
